dee225e835907f5b6b8e85b489782aeed5c2d0de
ku0100 <kuwilliams1@gmail.com>
2017-06-15 10:38:18 -0400

    website that will provide a random quote of the day. Still need twitter functionality

---
+++ randomQuote.js
@@ -0,0 +1,15 @@
+function getQuote() {
+  var ajaxRequest = new XMLHttpRequest();
+  ajaxRequest.open("GET", "https://andruxnet-random-famous-quotes.p.mashape.com/", false);
+  ajaxRequest.setRequestHeader("X-Mashape-Key", "ugRAQTYUqFmshNtS8wkkvxVjGyfrp1LbIKYjsnnZpJLFaoFWPz");
+  ajaxRequest.send(); // this is the authentication key provided at the site, need this or will not successfully connect!!
+
+  var json_obj = JSON.parse(ajaxRequest.responseText); // this will be the actual entire JSON object with all brackets and attributes
+  var randomQuote = json_obj.quote;
+  var quoteAuthor = json_obj.author;
+
+  $(document).ready(function() {
+    document.getElementById("quoteText").innerHTML = "\"" + randomQuote + "\"";
+    document.getElementById("quoteAuthor").innerHTML = "-" + quoteAuthor;
+  })
+};
